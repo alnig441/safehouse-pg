2d50fff687174487a179628bc102fb00259a20ea
alnig441 <alnig441@gmail.com>
2015-09-23 16:27:51 -0500

    bootstrap added, prelim styling applied to login page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('in index route');
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 module.exports = router;

2a2c295d93e777d8765199f86fa8533201751a88
alnig441 <alnig441@gmail.com>
2015-11-17 10:54:32 -0600

    general clean-up

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gutil = require('gulp-util');
 var jade = require('gulp-jade');
 var watch = require('gulp-watch');
 var minify = require('gulp-minify-css');
+var vendors = ['./node_modules/ng-file-upload/dist/ng-file-upload.min.js', './node_modules/ng-file-upload/dist/ng-file-upload-shim.min.js'];
 
 gulp.task('jade-templates', function(){
     gulp.src('./private/jade_templates/*.jade')
@@ -17,15 +18,22 @@ gulp.task('watch', function(){
     gulp.watch('./private/jade_templates/*.jade', ['jade-templates']);
 })
 
-gulp.task('default',['watch', 'move-js'], function(){
+gulp.task('default',['watch', 'move-vendors'], function(){
     gulp.start('jade-templates');
 });
 
+gulp.task('move-vendors', function(){
+    gulp.src(vendors)
+        .pipe(gulp.dest('./public/vendors/'))
+})
+
+/*
 gulp.task('move-js', function(){
-    return gulp.src('./private/javascripts/*.js')
+    return gulp.src('./private/javascripts/!*.js')
         .pipe(sourcemaps.init())
         .pipe(concat('app.min.js'))
         .pipe(uglify())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./public/javascripts'));
 });
+*/

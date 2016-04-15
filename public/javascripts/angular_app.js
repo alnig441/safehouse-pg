7e48a912b9d8ff065b5a53355f5240700de6b978
alnig441 <alnig441@gmail.com>
2016-04-15 15:41:24 -0500

    logging

--- public/javascripts/angular_app.js
+++ public/javascripts/angular_app.js
@@ -230,7 +230,7 @@ app.controller('privCtrl', ['$scope','$rootScope', '$http', '$log', '$modal', '$
         $scope.query.option = 'year';
         $scope.query.database = x;
 
-        console.log('QUERY: ', $scope.query);
+        console.log('QUERY: ', 'this is a string');
 
         $http.post('/event_crud/date', $scope.query)
             .then(function(response){

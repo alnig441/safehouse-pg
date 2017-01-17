aef07f60d58091d023340d57736e828d59f6a326
alnig441 <alnig441@gmail.com>
2017-01-16 21:53:31 -0600

    geo location data generation amended

--- public/javascripts/concatenated_app_file.js
+++ public/javascripts/concatenated_app_file.js
@@ -306,9 +306,10 @@ function capitalize (elem, ind, arr){
                 for(;i < elem.types.length; i++){
                     if(elem.types[i] === target){
                         element = elem;
+                        return element;
                     }
                 }
-                return element;
+                //return element;
 
             })
             //return element;
